80003945f41a5416325498061177fa6076e735cb
Paul Bzonek <38144013+pbzonek@users.noreply.github.com>
2025-07-07 17:29:39 -0400

    v1.3.1 - Pull SAVM from GitHub
    Remove local file, and add code to retrieve SAVM from Github repo instead.

--- 01 - Scripts/Script0-2_SAVM_setup.R
+++ 01 - Scripts/Script0-2_SAVM_setup.R
@@ -30,5 +30,29 @@ library(tools)
 library(units)
 library(utils)
 library(viridis)
+library(git2r) #Pull in recent package version 
+library(usethis) # Add a safety question
 
-devtools::load_all("SAVM_v0.0.1")
+# Remove old version if needed
+temp_folder_path <- "04 - Package/SAVM"
+# Check if the folder exists
+if (dir.exists(temp_folder_path)) {
+  # Ask the user to confirm deletion
+  if (ui_yeah(paste0("Folder '", temp_folder_path, "' already exists. Overwrite it?"))) {
+    unlink(temp_folder_path, recursive = TRUE, force = TRUE)
+    ui_done("Folder deleted. Proceeding.")
+  } else {
+    ui_oops("Operation cancelled by user.")
+    stop("Aborted to avoid overwriting.")
+  }
+}
+# download SAVM from Github
+git2r::clone("https://github.com/FishEcologyScience/SAVM.git", local_path = "04 - Package/SAVM")
+
+#Install SAVM
+devtools::load_all("04 - Package/SAVM")
+
+
+
+
+
